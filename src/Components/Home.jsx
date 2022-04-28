47452afecc5c3ba303c39c692d642dbcd297d820
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-04-28 20:22:44 +0530

    code

---
+++ src/Components/Home.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export function Home() {
+  
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="outlined-name"
+        label="Name"
+        value={name}
+        onChange={handleChange}
+      />
+      <TextField
+        id="outlined-uncontrolled"
+        label="Uncontrolled"
+        defaultValue="foo"
+      />
+    </Box>
+  );
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav , Navbar,NavDropdown, Container ,Button , Form , FormControl } from "react-bootstrap"
+
+export const NavbarIs = () => {
+
+
+    return(
+        <Navbar  bg="light" expand="lg">
+  <Container className='navbar' fluid>
+    <Navbar.Brand href="#">FLIGHT-FIGHT</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
