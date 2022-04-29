4805f9081fde48a6131f510d3a02ea4d35c93381
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-04-30 01:49:15 +0530

    redux incomplete

---
+++ src/Redux/flight/action.js
@@ -0,0 +1,24 @@
+
+export const FLIGHT_LOADING = "FLIGHT_LOADING"
+export const FLIGHT_SUCCESS = "FLIGHT_SUCCESS"
+export const FLIGHT_ERROR = "FLIGHT_ERROR"
+
+const FlightLoading = () => ({
+
+    type : FLIGHT_LOADING
+})
+
+const FlightSuccess = () =>({
+
+    type : FLIGHT_SUCCESS,
+    payload : payload
+})
+
+const FlightError = () => ({
+
+    type : FLIGHT_ERROR
+})
+
+export const FlightData = () => {
+    
+}
